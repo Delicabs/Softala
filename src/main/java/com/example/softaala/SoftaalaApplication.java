9daca9fda9030abf1b53afa78946887bc668d6e5
Juan Cabria <Juan.Cabria@gmail.com>
2019-03-25 09:57:10 +0200

    first commit

---
+++ src/main/java/com/example/softaala/SoftaalaApplication.java
@@ -0,0 +1,13 @@
+package com.example.softaala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftaalaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftaalaApplication.class, args);
+    }
+
+}
