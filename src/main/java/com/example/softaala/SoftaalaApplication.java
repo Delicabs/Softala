f1bb5dfd650320135ddc3e3f77d0961d31570b70
Snile Hydy <nakkimukiNSBM666>
2019-03-26 14:51:17 +0200

    //testi

--- src/main/java/com/example/softaala/SoftaalaApplication.java
+++ src/main/java/com/example/softaala/SoftaalaApplication.java
@@ -3,7 +3,7 @@ package com.example.softaala;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//asdasd
+
 
 // Testi Niilolle --
 //testi niilo
