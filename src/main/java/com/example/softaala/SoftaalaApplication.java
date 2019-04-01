71b3f758c6c2dd95e0c5ffc7589526c5f0cb7e5d
Ville Hotakainen <ville.hotakainen@gmail.com>
2019-04-01 11:05:48 +0300

    Merge branch 'master' of D:\Backend\Softala with conflicts.

--- src/main/java/com/example/softaala/SoftaalaApplication.java
+++ src/main/java/com/example/softaala/SoftaalaApplication.java
@@ -7,15 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-//testi_homma2
-//testi_homma
-// Testi Niilolle --
-//testi niilo
-//testi testi testi tämä on testi terveisin Erkki
-//Testi Jontte 1 2 3
-//Testees
-
-
 @SpringBootApplication
 public class SoftaalaApplication {
 
--- src/main/java/com/example/softaala/web/SoftaController.java
+++ src/main/java/com/example/softaala/web/SoftaController.java
@@ -5,35 +5,33 @@ import com.example.softaala.domain.FormRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
 @Controller
 public class SoftaController {
 
+    private EntityManager manager;
 
     @Autowired
     FormRepository formRepository;
 
     // RESTful all questions
-    @RequestMapping(value = "/questions")
+    @RequestMapping(value = "/questions", method = RequestMethod.GET)
     public @ResponseBody
     List<Form> formListRest() {
         return (List<Form>) formRepository.findAll();
     }
 
-    @RequestMapping(value = "/form/{id}", method = RequestMethod.GET)
+    //RESTfull service to get form id
+    @RequestMapping(value="/form/{id}", method = RequestMethod.GET)
     public @ResponseBody
-    Optional<Form> findQuestionRest(@PathVariable("id") Long formId) {
+    Optional<Form> findFormRest(@PathVariable("id") long formId) {
         return formRepository.findById(formId);
     }
-
-
 
     @RequestMapping("/index")
     public String index(Model model) {
@@ -43,5 +41,4 @@ public class SoftaController {
         model.addAttribute("hello", hello);
         return "index";
     }
-
 }
