fb48156090e0c4d67befacd5f3863fd549bd2080
Snile Hydy <nakkimukiNSBM666>
2019-04-02 15:23:19 +0300

    // question/repository/jsonmapper

---
+++ src/main/java/com/example/softaala/domain/QuestionsRepository.java
@@ -0,0 +1,12 @@
+package com.example.softaala.domain;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface QuestionsRepository extends CrudRepository<Questions, Long> {
+    List<Questions> findById(@Param("Form")String question);
+
+
+}
