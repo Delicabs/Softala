f97d43dfe22892a72ede6fe106e34a246f2cdb22
Juan Cabria <Juan.Cabria@gmail.com>
2019-04-15 09:44:37 +0300

    Commit

--- src/main/java/com/example/softaala/domain/Questions.java
+++ src/main/java/com/example/softaala/domain/Questions.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 @Entity
 public class Questions {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String question;
 
